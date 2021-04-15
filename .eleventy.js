9790107c9197f8772019385eb2c3943c3dbad363
Shua11 <math.joshua11@gmail.com>
2021-04-15 08:43:18 -0600

    small update

--- .eleventy.js
+++ .eleventy.js
@@ -5,16 +5,16 @@ const fs = require('fs-extra')
 module.exports = function (eleventyConfig) {
 
     // Rough method to clean out the dist folder...
-    fs.remove('dist')
-    .then(() => {
-    console.log('success!')
-    })
-    .catch(err => {
-    console.error(err)
-    })
-    
-    eleventyConfig.addPassthroughCopy("src/images")
-    eleventyConfig.addPassthroughCopy("src/admin")
+    // fs.remove('dist')
+    // .then(() => {
+    // console.log('success!')
+    // })
+    // .catch(err => {
+    // console.error(err)
+    // })
+
+    eleventyConfig.addPassthroughCopy("images")
+    eleventyConfig.addPassthroughCopy("admin")
 
     return {
         dir: {
