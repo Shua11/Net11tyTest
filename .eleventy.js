75bb3d2c962a5995ff3557e78f58fbeb27dcc063
Shua11 <math.joshua11@gmail.com>
2021-04-15 08:41:43 -0600

    changing structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,24 @@
-// const fs = require('fs-extra')
+const fs = require('fs-extra')
 
-// Rough method to clean out the dist folder...
-// fs.remove('dist')
-// .then(() => {
-//   console.log('success!')
-// })
-// .catch(err => {
-//   console.error(err)
-// })
+
 
 module.exports = function (eleventyConfig) {
+
+    // Rough method to clean out the dist folder...
+    fs.remove('dist')
+    .then(() => {
+    console.log('success!')
+    })
+    .catch(err => {
+    console.error(err)
+    })
+    
     eleventyConfig.addPassthroughCopy("src/images")
     eleventyConfig.addPassthroughCopy("src/admin")
 
     return {
         dir: {
-            input: "src",
+            // input: "src",
             output: "dist"
         }
     };
