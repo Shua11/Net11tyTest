88988d2d16b6ea85656b7a3beab2cf074f316883
Shua11 <math.joshua11@gmail.com>
2021-04-22 16:26:37 -0600

    small tests

--- .eleventy.js
+++ .eleventy.js
@@ -5,15 +5,14 @@ const fs = require('fs-extra')
 module.exports = function (eleventyConfig) {
 
     // Rough method to clean out the dist folder...
-    fs.remove('dist')
-    .then(() => {
-    console.log('success!')
-    })
-    .catch(err => {
-    console.error(err)
-    })
+    // fs.remove('dist')
+    // .then(() => {
+    // console.log('success!')
+    // })
+    // .catch(err => {
+    // console.error(err)
+    // })
 
-    eleventyConfig.addPassthroughCopy("src/images")
     eleventyConfig.addPassthroughCopy("src/admin")
     eleventyConfig.addPassthroughCopy("src/style")
     eleventyConfig.addPassthroughCopy("src/assets")
